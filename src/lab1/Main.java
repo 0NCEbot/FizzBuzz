dbbb66b24cced68af9318a42b2245709d8c3eb8b
0NCEbot <danbhnguyen2006@gmail.com>
2025-09-08 14:18:22 -0400

    created and completed Main file (task 3)

---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        System.out.println();
+        FizzBuzz.main(args);
+    }
+}
