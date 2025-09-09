e2d0de6fc22a690bc5752230db7aeb3ce82fbe24
0NCEbot <danbhnguyen2006@gmail.com>
2025-09-08 20:03:38 -0400

    completed task 4.2

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,29 @@
 package lab1;
 
 public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            boolean divisibleByA = i % a == 0;
+            boolean divisibleByB = i % b == 0;
+
+            if (divisibleByA || divisibleByB) count++;
+        }
+        return count;
+    }
+
+    public static int multiples() {
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean divisibleByA = i % 3 == 0;
+            boolean divisibleByB = i % 5 == 0;
+
+            if (divisibleByA || divisibleByB) count++;
+        }
+        return count;
+    }
 }
