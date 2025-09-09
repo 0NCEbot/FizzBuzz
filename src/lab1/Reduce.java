e26b90f2427083034af8bc0dde96cb7dc11807fa
0NCEbot <danbhnguyen2006@gmail.com>
2025-09-08 22:54:00 -0400

    bonus question, pre-extraction

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,16 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int count = 0;
+        while (n != 0) {
+            boolean even = n % 2 == 0;
+
+            if (even) n /= 2;
+            else n -= 1;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
